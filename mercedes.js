aa809b91596c579129faa012c1dde202b504b5b4
laviedegeorge <laviedegeorge1292@gmail.com>
2019-10-09 07:14:15 +0100

    almost there...

--- mercedes.js
+++ mercedes.js
@@ -1,5 +1,6 @@
-let counter_1 = -1;
-let counter = -1;
+
+let counter = 0;
+const numbers = document.querySelectorAll('[id=numbers] p');
 
 const cars = [
     {   
@@ -43,99 +44,87 @@ const cars = [
         acceleration: '0 - 100km/h in 4.5s' 
     }
 ]
-
-function updateData({ name,image,torque,power,engine,acceleration }) {
-    let nameHolder = document.getElementById('name');
-    let imageHolder = document.getElementById('image');
-    /* ---TORQUE NUMBERS------ */
+/* -----------UPDATE DATA WITH INFO FROM CARS ARRAY------------ */
+const torque = () => {
     let torqueNum1 = document.getElementById('torqueNum1');
     let torqueNum2 = document.getElementById('torqueNum2');
     let torqueNum3 = document.getElementById('torqueNum3');
-    /* let torqueHolder = document.getElementById('torque'); */
 
-    /* ---POWER NUMBERS------ */
-    let powerNum1 = document.getElementById('powerNum1');
-    let powerNum2 = document.getElementById('powerNum2');
-    let powerNum3 = document.getElementById('powerNum3');
-    /* let powerHolder= document.getElementById('power'); */
+    let torqueNums = Array.from(cars[counter].torque);
 
-    let engineHolder = document.getElementById('engine');
-    let accelerationHolder = document.getElementById('acceleration');
-
-    if (counter_1 == (cars.length -1)) {
-        counter_1 = 0;
-    } else {
-        counter_1 ++;
-    }
-
-    let powerNums = Array.from(cars[counter_1].power);
-    let torqueNums = Array.from(cars[counter_1].torque);
-
-
-    nameHolder.innerHTML = cars[counter_1].name;
-    imageHolder.src = cars[counter_1].image;
-    imageHolder.classList.add("scaling");
-
-    
-    /* ---POWER NUMBERS------ */
-
-    /* torqueHolder.innerHTML = cars[counter_1].torque; */
     torqueNum1.style.top = `-${torqueNums[0] -1}em`;
     torqueNum2.style.top = `-${torqueNums[1] -1}em`;
     torqueNum3.style.top = `-${torqueNums[2] -1}em`;
-    
-    /* ---POWER NUMBERS------ */
+}
+
+const power = () => {
+    let powerNum1 = document.getElementById('powerNum1');
+    let powerNum2 = document.getElementById('powerNum2');
+    let powerNum3 = document.getElementById('powerNum3');
+
+    let powerNums = Array.from(cars[counter].power);
+
     powerNum1.style.top = `-${powerNums[0] -1}em`;
     powerNum2.style.top = `-${powerNums[1] -1}em`;
     powerNum3.style.top = `-${powerNums[2] -1}em`;
-    /* powerHolder.innerHTML = cars[counter_1].power; */
+}
 
-    engineHolder.innerHTML = cars[counter_1].engine;
-    accelerationHolder.innerHTML = cars[counter_1].acceleration;
+function updateData({ name,image,engine,acceleration }) {
+    let nameHolder = document.getElementById('name');
+    let imageHolder = document.getElementById('image');
+    let engineHolder = document.getElementById('engine');
+    let accelerationHolder = document.getElementById('acceleration');
+
+    nameHolder.innerHTML = cars[counter].name;
+    imageHolder.src = cars[counter].image;
+    imageHolder.classList.add("scaling");
+    torque();
+    power();
+    engineHolder.innerHTML = cars[counter].engine;
+    accelerationHolder.innerHTML = cars[counter].acceleration;
 
     setTimeout(() => {
         imageHolder.classList.remove('scaling');
     }, 500);
 }
 
-const increment = () => {
-    const numbers = document.querySelectorAll('[id=numbers] p');
+const updateCars = () => {
     numbers.forEach(el => el.classList.remove('selected'))
-    
-    if (counter == (numbers.length - 1) ) {
-        counter = 0;
-    } else {
-        counter ++;
-    }
-
     updateData(cars[counter]);
     numbers[counter].classList.add('selected');
 }
 
-const decrement = () => {
-    
-    /* console.log('clicked');
-    const numbers = document.querySelectorAll('[id=numbers] p');
-    numbers.forEach(el => el.classList.remove('selected'))
-    
-    if (counter != numbers.length) {
-        counter --;
-    } else {
-        counter = 0;
+const checker = (e) => {
+    e.preventDefault();
+    const up = document.getElementById('up');
+    const down = document.getElementById('down');
+
+    /* UP */
+     if (e.target == up) {
+         if (counter > 0) {
+            counter --;
+        } else {
+            counter = 0;
+        }
+        console.log(counter);
     }
-    console.log(numbers[counter])
-    numbers[counter].classList.add('selected');
-    
-    console.log(counter); */
+
+    /* DOWN */
+     if (e.target == down) {
+        if (counter == (numbers.length - 1) ) {
+            counter = 0;
+        } else {
+            counter ++;
+        }
+       console.log(counter);
+    }
+    updateCars();
 }
 
-const down = document.getElementById('down');
-down.addEventListener('click', increment);
+const sideNav = document.getElementById('side-nav');
+sideNav.addEventListener('click', checker);
 
-const up = document.getElementById('up');
-down.addEventListener('click', decrement);
-
-/* window.onload = () => {
-    increment();
-}; */
+window.onload = () => {
+    updateCars();
+};
 
